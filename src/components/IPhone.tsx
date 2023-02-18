187263bfd5cbebfc177125ca5970340e95de2c6d
Younger Jo <youngerjo@gmail.com>
2023-02-18 14:58:18 +0900

    Change bundler to metro

--- src/components/IPhone.tsx
+++ src/components/IPhone.tsx
@@ -1,8 +1,11 @@
 import { useFrame } from '@fiber'
 import { useGLTF, type GLTF } from '@drei'
+import { Asset } from 'expo-asset'
+
+const asset = Asset.fromModule(require('@assets/models/iphone.glb'))
 
 export default function IPhone(props: any) {
-  const { scene } = useGLTF(require('@assets/models/iphone.glb')) as GLTF
+  const { scene } = useGLTF(asset.uri) as GLTF
 
   useFrame(() => {
     scene.rotation.y += 0.01
