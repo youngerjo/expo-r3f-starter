719ab5b71afcc57a5c7cd8b654a68bdb06cb3b70
Younger Jo <youngerjo@gmail.com>
2023-12-01 16:45:39 +0900

    Update to Expo 49

--- app/demo.tsx
+++ app/demo.tsx
@@ -4,7 +4,8 @@ import { Canvas, useFrame } from '@lib/fiber'
 import { Environment, useGLTF, type GLTF } from '@lib/drei'
 import { EffectComposer, Bloom } from '@lib/postprocessing'
 
-const asset = Asset.fromModule(require('@assets/models/iphone.glb'))
+// const asset = Asset.fromModule(require('@assets/models/iphone.glb'))
+const asset = Asset.fromModule(require('@assets/models/iphone.gltf'))
 
 function IPhone(props: any) {
   const { scene } = useGLTF(asset.uri) as GLTF
@@ -18,7 +19,7 @@ function IPhone(props: any) {
 
 export default function Page() {
   return (
-    <Canvas gl={{ physicallyCorrectLights: true }} camera={{ position: [-6, 0, 16], fov: 36 }}>
+    <Canvas gl={{ useLegacyLights: false }} camera={{ position: [-6, 0, 16], fov: 36 }}>
       <color attach="background" args={[0xe2f4df]} />
       <ambientLight />
       <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
